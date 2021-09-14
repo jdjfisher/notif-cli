c957230495111aed89a495e70019dc30ea7474a2
JDJFisher <jordanfisher2096@gmail.com>
2021-09-14 16:58:23 +0100

    Added basic QR scanning

--- python/notif.py
+++ python/notif.py
@@ -5,6 +5,9 @@ import os.path
 import requests
 import socket
 import json
+import io
+import sys
+from qrcode import QRCode
 
 API_URL = 'http://localhost:8000'
 APP_NAME = 'notif'
@@ -18,7 +21,7 @@ def main(argv=None) -> None:
   # parser.add_argument('-v', '--verbose', help='Verbose', action='store_true')
 
   # Positional args
-  parser.add_argument('action', help='...', type=str, choices=['bind', 'ping', 'unbind'])
+  parser.add_argument('action', help='...', type=str, choices=['link', 'ping', 'unlink'], nargs='?', default='ping')
 
   # Invoke arg parser
   args = parser.parse_args(argv)
@@ -32,55 +35,66 @@ def main(argv=None) -> None:
       token = file.read()
 
   # Action
-  if action == 'bind':
-    bind(token)
+  if action == 'link':
+    link(token)
   elif action == 'ping':
     ping(token)
-  elif action == 'unbind':
-    unbind(token)
+  elif action == 'unlink':
+    unlink(token)
   else:
-    exit('fatal')
+    exit(f'fatal: unsupported action {action}')
 
   exit(0) 
 
 
-def bind(token: str) -> None:
+def link(token: str, device: str = None) -> None:
   if token:
-    unbind(token)
+    unlink(token)
 
   response = requests.get(f'{API_URL}/token')
   
   token = json.loads(response.text)['token']
 
-  device = socket.gethostname() # TODO: Make this an optional flag and validate
+  if not (device):
+    device = socket.gethostname() 
 
   with open(TOKEN_PATH, 'w') as file:
     file.write(token)
 
-  # url = f'{APP_NAME}://bind?device={device}&token={token}'
+  payload = {
+    'name': device,
+    'token': token,
+  }
 
-  print(f'device: {device}, token: {token}')
-  print('TODO: generate and display a QR for the app')
+  qr = QRCode()
+  qr.add_data(json.dumps(payload)) 
+  qr.print_ascii(out=sys.stdout)
+
+  # while True:
+  #   input()
 
 
-def ping(token: str) -> None:
+def ping(token: str, message: str = None) -> None:
   if not(token):
-    exit('please bind first')
+    exit('please link first')
 
-  payload = { 'token': token }
+  payload = { 
+    'token': token,
+    'message': message,
+  }
 
   response = requests.post(f'{API_URL}/ping', json=payload)
   if not(response.ok):
     exit(response)
 
 
-def unbind(token: str) -> None:
+def unlink(token: str) -> None:
   if not(token):
     exit('not bound')
 
   payload = { 'token': token }
 
-  response = requests.post(f'{API_URL}/unbind', json=payload)
+  response = requests.post(f'{API_URL}/unlink', json=payload)
   if not(response.ok):
     exit(response)
 
