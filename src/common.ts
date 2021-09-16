9ab9bab69a0c19901d022c96c54103609428ca3e
JDJFisher <jordanfisher2096@gmail.com>
2021-09-16 18:11:59 +0100

    Basic linking working

--- src/common.ts
+++ src/common.ts
@@ -13,13 +13,36 @@ export const api = axios.create({
 });
 
 export const getToken = (): string | undefined => {
-  return fs.readFileSync(CONFIG_PATH, 'utf8');
+  // TODO: Redo, check if file exists
+  try {
+    return fs.readFileSync(CONFIG_PATH, 'utf8');
+  } catch (error) {
+    return undefined;
+  }
 }
 
 export const setToken = (token?: string): void => {
-  fs.writeFileSync(CONFIG_PATH, token,{ encoding: 'utf8', flag: 'w' });
+  if (token) {
+    fs.writeFileSync(CONFIG_PATH, token, { encoding: 'utf8', flag: 'w' });
+  } else {
+    fs.unlinkSync(CONFIG_PATH);
+  }
 }
 
-export const openSocket = (): Socket => {
-  return io(API_URL);
+export const openSocket = async (timeout: number = 1000): Promise<Socket> => {
+  return new Promise((resolve, reject) => {
+    const socket = io(API_URL);
+    let connected = false;
+
+    socket.on('connect', () => {
+      connected = true;
+      resolve(socket);
+    });
+
+    setTimeout(() => {
+      if (!connected)
+        reject();
+    }, timeout);
+  });
 }
+
--- src/notif-link.ts
+++ src/notif-link.ts
@@ -10,7 +10,7 @@ const os = require("os");
 
   program
     .option('-f, --force', 'force override link');
-  // TODO: Option to view QR as png
+    // .option('-n, --name', 'speicfy a custom name for this device');
 
   program.parse();
 
@@ -19,37 +19,54 @@ const os = require("os");
   if (token) {
     if (!program.opts().force) 
       throw Error('already linked. use -f to override');
-    // else 
-      // TODO: unlink
+    else 
+      // TODO: unlink server-side
+      setToken(undefined);
   }
 
   const response = await api.get('token');
 
   token = response.data.token;
 
+  // TODO: Handle error
+  const socket = await openSocket();
+
   const payload = JSON.stringify({
     token: token,
     name: os.hostname(),
+    socketId: socket.id,
   });
   
-  setToken(token);
-
   const qr = await QRCode.toString(payload, {type: 'terminal'});
+
   console.log(qr);
 
-  // const socket = openSocket();
-  // const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-  // let linked = false; 
-  // socket.on('linked', () => {
-  //   linked = true;
-  // });
+  console.log('Waiting for link. Ctrl+C to cancel');
 
-  // for (let i = 0; i < 10; i++){
-  //   if (linked)
-  //     break;
+  const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+  let linked = false; 
 
-  //   await delay(1000);
-  // }
+  socket.on('linked', () => {
+    linked = true;
+  });
 
-  // console.log(linked ? 'device linked', 'timeout reached');
+  for (let i = 0; i < 1000; i++){
+    if (linked)
+      break;
+
+    console.log('hi');
+
+    await delay(1000);
+  }
+
+  console.clear();
+
+  if (!linked) {
+    console.log('link expired');
+    return;
+  }
+
+  setToken(token);
+  
+  console.log('device linked');
 })();
