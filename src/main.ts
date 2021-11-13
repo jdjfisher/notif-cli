db5f0c1f7cf7f94c66f21a503c779c74802d4b8e
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2021-11-13 17:12:24 +0000

    Cli pathing bugfix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
+import path from 'path';
 import dotenv from 'dotenv';
 import program from './notif';
 
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '../.env') });
 program.parse(process.argv);
