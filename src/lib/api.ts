047509391aa23b47c09bd097ff9cbede957e4fef
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2023-04-07 15:19:51 +0100

    Switched socket io for pusher

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import io, { Socket } from 'socket.io-client';
 
 export const API_URL: string = process.env.API_URL || 'https://notif.jdjfisher.dev';
 
@@ -19,19 +18,3 @@ export const createApiClient = ({
     },
     timeout: 1000,
   });
-
-export const openSocket = async (url?: string, timeout = 1000): Promise<Socket> => {
-  return new Promise((resolve, reject) => {
-    const socket = io(url || API_URL);
-    let connected = false;
-
-    socket.on('connect', () => {
-      connected = true;
-      resolve(socket);
-    });
-
-    setTimeout(() => {
-      if (!connected) reject();
-    }, timeout);
-  });
-};
